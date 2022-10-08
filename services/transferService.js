4bd421fa327f7ef3eccca546ab69c9ad00408b1c
lmaxxx <leshchenko.maksym10@gmail.com>
2022-10-08 23:27:50 +0200

    feat: Create process transfer function

--- services/transferService.js
+++ services/transferService.js
@@ -1,5 +1,6 @@
 const Transfer = require("../models/Transfer")
 const DataService = require("../services/dataService")
+const Account = require("../models/Account")
 
 class TransferService {
   async getTransfers(userId) {
@@ -13,7 +14,23 @@ class TransferService {
       userId, from, to, amount
     })
 
-    return DataService.getTransferFromDoc(transferDoc)
+    const transfer = DataService.getTransferFromDoc(transferDoc)
+    this.processTransfer(transfer)
+
+    return transfer
+  }
+
+  async processTransfer(transfer) {
+    const {from, to, amount: transferAmount} = transfer
+    const {currency: fromCurrency, id: fromId, amount: fromAmount} = from
+    const {currency: toCurrency, id: toId, toAmount} = to
+
+    if(fromCurrency === toCurrency) {
+      await Account.findByIdAndUpdate(fromId, {amount: fromAmount - transferAmount})
+      await Account.findByIdAndUpdate(toId, {amount: toAmount + transferAmount})
+
+      return
+    }
   }
 }
 
