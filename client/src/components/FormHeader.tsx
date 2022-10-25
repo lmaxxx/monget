bc6eea3c3ca1e928215dcbb3ff19bd6e1e3d3466
lmaxxx <leshchenko.maksym10@gmail.com>
2022-10-25 15:07:43 +0200

    feat: Create 'FormHeader' component for each form

---
+++ client/src/components/FormHeader.tsx
@@ -0,0 +1,22 @@
+import {FC} from 'react'
+import {CloseButton, Group, Title} from "@mantine/core";
+import {useNavigate} from "react-router-dom";
+
+interface PropsType {
+  title: string
+  goBackPath: string
+}
+
+const FormHeader: FC<PropsType> = ({title, goBackPath}) => {
+  const navigate = useNavigate()
+  const goBack = () => navigate(goBackPath)
+
+  return (
+    <Group position={"apart"}>
+      <Title mb={"sm"} order={2} sx={{fontSize: "2rem", fontWeight: 500}} align={"center"}>{title}</Title>
+      <CloseButton color={"red"} size={"lg"} onClick={goBack} aria-label="Close transfers" />
+    </Group>
+  )
+}
+
+export default FormHeader
--- client/src/pages/Transfers.tsx
+++ client/src/pages/Transfers.tsx
@@ -1,12 +1,10 @@
 import {Center, CloseButton, Group, Paper, Title, useMantineTheme} from "@mantine/core";
 import {useNavigate} from "react-router-dom";
 import TransferList from "../components/TransferList";
+import FormHeader from "../components/FormHeader";
 
 const Transfers = () => {
   const theme = useMantineTheme()
-  const navigate = useNavigate()
-
-  const goBack = () => navigate("/accounts")
 
   return (
     <Center style={{
@@ -15,10 +13,7 @@ const Transfers = () => {
       backgroundColor: theme.colors.gray[1]
     }}>
       <Paper sx={{maxWidth: 400, width: "90%"}} shadow="xl" radius="md" p="xl">
-        <Group position={"apart"}>
-          <Title mb={"sm"} order={2} sx={{fontSize: "2rem", fontWeight: 500}} align={"center"}>Transfers</Title>
-          <CloseButton size={"lg"} onClick={goBack} aria-label="Close transfers" />
-        </Group>
+        <FormHeader title={"Transfers"} goBackPath={"/accounts"} />
         <TransferList/>
       </Paper>
     </Center>
