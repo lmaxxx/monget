9aa90931d2d9b0b96ec4da5394aaf7ddf795ade8
lmaxxx <leshchenko.maksym10@gmail.com>
2022-11-15 10:42:54 +0100

    fix: Rename CreateTransacionForm to TransactionCreateForm

--- client/src/pages/TransactionCreate.tsx
+++ client/src/pages/TransactionCreate.tsx
@@ -1,6 +1,6 @@
 import {Center, Paper, useMantineTheme} from "@mantine/core";
 import FormHeader from "../components/FormHeader";
-import CreateTransactionForm from "../components/CreateTransactionForm";
+import TransactionCreateForm from "../components/TransactionCreateForm";
 
 const TransactionCreate = () => {
   const theme = useMantineTheme()
@@ -12,8 +12,8 @@ const TransactionCreate = () => {
       backgroundColor: theme.colors.gray[1]
     }}>
       <Paper sx={{maxWidth: 600, width: "90%"}} shadow="xl" radius="md" p="xl">
-        <FormHeader title={"Create transaction"} goBackPath={"/categories"}/>
-        <CreateTransactionForm/>
+        <FormHeader title={"Create transaction"} goBackPath={"/"}/>
+        <TransactionCreateForm/>
       </Paper>
     </Center>
   )
