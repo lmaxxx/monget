07144d35096548bdbbd94b524ec5f0cdce336f23
lmaxxx <leshchenko.maksym10@gmail.com>
2022-11-27 01:09:47 +0100

    fix: Fix FormHeader positioning

---
+++ client/src/components/FormHeader.tsx
@@ -0,0 +1,28 @@
+import {FC} from 'react'
+import {CloseButton, Box, Title} from "@mantine/core";
+import {useNavigate} from "react-router-dom";
+import HiddenTextStyles from "../assets/hiddenTextStyles";
+
+interface PropsType {
+  title: string
+  goBackPath: string
+}
+
+const FormHeader: FC<PropsType> = ({title, goBackPath}) => {
+  const navigate = useNavigate()
+  const goBack = () => navigate(goBackPath)
+
+  return (
+    <Box sx={{
+      display: "grid",
+      gridTemplateColumns: "auto 34px",
+      alignItems: "center",
+      gap: "1rem",
+    }}>
+      <Title order={2} sx={{fontSize: "1.5rem", fontWeight: 500, ...HiddenTextStyles}}>{title}</Title>
+      <CloseButton color={"red"} size={"lg"} onClick={goBack} aria-label="Close transfers" />
+    </Box>
+  )
+}
+
+export default FormHeader
