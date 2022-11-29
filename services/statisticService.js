01d5e75c92c9956a2fa87f0e11ddce6cc268473f
lmaxxx <leshchenko.maksym10@gmail.com>
2022-11-29 17:17:58 +0100

    feat: Create week statistic endpoint

--- services/statisticService.js
+++ services/statisticService.js
@@ -6,6 +6,7 @@ class StatisticService {
   async getStatistics(queryType, dateCounter, transactionType) {
     switch (queryType) {
       case "years": return await this.getYearStatistic(dateCounter, transactionType)
+      case "weeks": return await this.getWeekStatistic(dateCounter, transactionType)
     }
   }
 
@@ -19,17 +20,16 @@ class StatisticService {
       end: endOfTheYear
     }
 
-    const transactions = await TransactionService.getTransactions(null, transactionType, query, {})
+    const transactions = await TransactionService.getTransactions(null, transactionType, query)
 
     for(let i = 0; i < 12; i++) {
       const startOfTheMonth = DateService.getStartOfTheMonthByIndex(startOfTheYear, i)
       const endOfTheMonth = DateService.getEndOfTheMonthByIndex(startOfTheYear, i)
-      const monthName = startOfTheMonth.toLocaleString('en-US', {
-        month: 'short',
-      });
+      const monthName = startOfTheMonth.toLocaleString('en-US', {month: 'short'})
       const monthData = {label: monthName, expenses: 0, income: 0, profit: 0, loss: 0}
       const monthTransactions = transactions.filter(transaction => (
-        transaction.date.getTime() >= startOfTheMonth.getTime() && transaction.date.getTime() <= endOfTheMonth.getTime()
+        transaction.date.getTime() >= startOfTheMonth.getTime() &&
+        transaction.date.getTime() <= endOfTheMonth.getTime()
       ))
 
       monthTransactions.forEach(transaction => {
@@ -50,6 +50,52 @@ class StatisticService {
       }
 
       data.push(monthData)
+    }
+
+    return data
+  }
+
+  async getWeekStatistic(dateCounter, transactionType) {
+    const data = []
+    const currentDate = new Date()
+    const weekStartDay = DateService.subtractDays(currentDate, dateCounter * 7)
+    const weekEndDay = DateService.addDays(weekStartDay, 7)
+
+    const query = {
+      start: DateService.getStartOfTheDay(weekStartDay),
+      end: DateService.getEndOfTheDay(weekEndDay),
+    }
+
+    const transactions = await TransactionService.getTransactions(null, transactionType, query)
+
+    for (let i = 0; i < 7; i++) {
+      const startOfTheDay = DateService.addDays(query.start, i + 1)
+      const endOfTheDay = DateService.getEndOfTheDay(startOfTheDay)
+      const dayName = startOfTheDay.toLocaleString('en-US', {weekday: 'short'})
+      const weekData = {label: dayName, expenses: 0, income: 0, profit: 0, loss: 0}
+      const weekTransactions = transactions.filter(transaction => (
+        transaction.date.getTime() >= startOfTheDay.getTime() &&
+        transaction.date.getTime() <= endOfTheDay.getTime()
+      ))
+
+      weekTransactions.forEach(transaction => {
+        if(transaction.transactionType === "expenses")  {
+          weekData.expenses += transaction.amount
+          return
+        }
+
+        weekData.income += transaction.amount
+      })
+
+      const profit = weekData.income - weekData.expenses
+
+      if(profit >= 0) {
+        weekData.profit = profit
+      } else {
+        weekData.loss = Math.abs(profit)
+      }
+
+      data.push(weekData)
     }
 
     return data
